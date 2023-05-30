5cb6c188b472fe9b44614baa0106e3c1a95e9a5a
itsmewt <104033731+itsmewt@users.noreply.github.com>
2023-05-30 10:33:18 -0300

    Update text_items_exams.js

--- src/components/exams/text_items_exams.js
+++ src/components/exams/text_items_exams.js
@@ -1,56 +1,37 @@
 export const itemTextsExams = {
   Hemograma:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget congue lacus. Mauris" +
-    "quis urna ultrices, convallis eros eget, eleifend massa. Integer at leo erat. Etiam volutpat id" +
-    "tortor varius suscipit. Phasellus et lacus nunc.\n" +
-    "Sed ac ultrices orci. Suspendisse ac massa at arcu aliquam lacinia eget non felis." +
-    "Praesent eget velit viverra, pulvinar urna id, finibus lacus. Aliquam congue sit amet libero in" +
-    "rutrum. Nulla bibendum rutrum bibendum. Integer eleifend dolor in finibus finibus. In tellus turpis," +
-    "maximus sit amet volutpat eu, luctus non orci. Fusce laoreet laoreet ullamcorper. Praesent ut ipsum" +
-    "sagittis, pulvinar risus eget, tincidunt nibh.",
+      "Descubra o segredo por trás da saúde do seu amiguinho! Com o exame de Hemograma, nossos especialistas analisam " +
+      "minuciosamente as células sanguíneas do seu pet. Eles desvendam os mistérios dos glóbulos vermelhos, brancos e " +
+      "plaquetas, fornecendo informações vitais sobre a saúde geral do seu peludo. Essa investigação detalhada permite " +
+      "um diagnóstico preciso e ajuda a garantir que seu amado animal esteja sempre cheio de vitalidade e energia.",
   Bioquímico:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget congue lacus. Mauris" +
-    "quis urna ultrices, convallis eros eget, eleifend massa. Integer at leo erat. Etiam volutpat id" +
-    "tortor varius suscipit. Phasellus et lacus nunc.\n" +
-    "Sed ac ultrices orci. Suspendisse ac massa at arcu aliquam lacinia eget non felis." +
-    "Praesent eget velit viverra, pulvinar urna id, finibus lacus. Aliquam congue sit amet libero in" +
-    "rutrum. Nulla bibendum rutrum bibendum. Integer eleifend dolor in finibus finibus. In tellus turpis," +
-    "maximus sit amet volutpat eu, luctus non orci. Fusce laoreet laoreet ullamcorper. Praesent ut ipsum" +
-    "sagittis, pulvinar risus eget, tincidunt nibh.",
+      "Aqui na nossa clínica, a ciência se une à saúde do seu animalzinho! Com o exame Bioquímico, nossos especialistas " +
+      "exploram os segredos escondidos nos fluidos corporais do seu pet. Eles analisam os níveis de açúcar, proteínas, " +
+      "enzimas e outras substâncias para avaliar o funcionamento dos órgãos e detectar possíveis desequilíbrios. Essa " +
+      "investigação laboratorial minuciosa permite um cuidado personalizado e ajuda a manter seu peludo saudável e feliz.",
   Eletrocardiograma:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget congue lacus. Mauris" +
-    "quis urna ultrices, convallis eros eget, eleifend massa. Integer at leo erat. Etiam volutpat id" +
-    "tortor varius suscipit. Phasellus et lacus nunc.\n" +
-    "Sed ac ultrices orci. Suspendisse ac massa at arcu aliquam lacinia eget non felis." +
-    "Praesent eget velit viverra, pulvinar urna id, finibus lacus. Aliquam congue sit amet libero in" +
-    "rutrum. Nulla bibendum rutrum bibendum. Integer eleifend dolor in finibus finibus. In tellus turpis," +
-    "maximus sit amet volutpat eu, luctus non orci. Fusce laoreet laoreet ullamcorper. Praesent ut ipsum" +
-    "sagittis, pulvinar risus eget, tincidunt nibh.",
+      "Entre no ritmo do coração do seu amado pet! Com o exame de Eletrocardiograma, nossos especialistas capturam a " +
+      "melodia elétrica do coração do seu peludo. Eles utilizam eletrodos especiais para registrar os impulsos " +
+      "elétricos que controlam os batimentos cardíacos, permitindo uma análise precisa da saúde cardiovascular. " +
+      "Essa sinfonia cardíaca fornece informações valiosas sobre ritmos irregulares, doenças cardíacas e garante " +
+      "que seu pet possa continuar dançando pela vida.",
   Ultrassonografia:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget congue lacus. Mauris" +
-    "quis urna ultrices, convallis eros eget, eleifend massa. Integer at leo erat. Etiam volutpat id" +
-    "tortor varius suscipit. Phasellus et lacus nunc.\n" +
-    "Sed ac ultrices orci. Suspendisse ac massa at arcu aliquam lacinia eget non felis." +
-    "Praesent eget velit viverra, pulvinar urna id, finibus lacus. Aliquam congue sit amet libero in" +
-    "rutrum. Nulla bibendum rutrum bibendum. Integer eleifend dolor in finibus finibus. In tellus turpis," +
-    "maximus sit amet volutpat eu, luctus non orci. Fusce laoreet laoreet ullamcorper. Praesent ut ipsum" +
-    "sagittis, pulvinar risus eget, tincidunt nibh.",
+      "Prepare-se para uma viagem ao interior do corpo do seu animalzinho! Com o exame de Ultrassonografia, " +
+      "]nossos especialistas utilizam ondas sonoras de alta frequência para criar imagens detalhadas dos " +
+      "órgãos internos do seu pet. Eles exploram cada canto e recanto, revelando os segredos escondidos no " +
+      "abdômen, no coração, no fígado e muito mais. Essa jornada ultrassônica permite uma avaliação precisa " +
+      "da saúde interna do seu peludo, garantindo que ele continue a explorar o mundo com curiosidade e vitalidade.",
   Urinálise:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget congue lacus. Mauris" +
-    "quis urna ultrices, convallis eros eget, eleifend massa. Integer at leo erat. Etiam volutpat id" +
-    "tortor varius suscipit. Phasellus et lacus nunc.\n" +
-    "Sed ac ultrices orci. Suspendisse ac massa at arcu aliquam lacinia eget non felis." +
-    "Praesent eget velit viverra, pulvinar urna id, finibus lacus. Aliquam congue sit amet libero in" +
-    "rutrum. Nulla bibendum rutrum bibendum. Integer eleifend dolor in finibus finibus. In tellus turpis," +
-    "maximus sit amet volutpat eu, luctus non orci. Fusce laoreet laoreet ullamcorper. Praesent ut ipsum" +
-    "sagittis, pulvinar risus eget, tincidunt nibh.",
+      "A saúde do seu pet está nas mãos do seu xixi! Com o exame de Urinálise, nossos especialistas " +
+      "investigam o líquido dourado do seu animalzinho em busca de pistas valiosas. Eles analisam a " +
+      "composição química, a presença de bactérias e outras substâncias, revelando informações cruciais " +
+      "sobre a saúde dos rins, do trato urinário e até mesmo sobre a hidratação do seu peludo. Essa análise " +
+      "minuciosa da urina garante que seu pet esteja sempre equilibrado e pronto para enfrentar as aventuras " +
+      "do dia a dia.",
   "Doenças Infecciosas":
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget congue lacus. Mauris" +
-    "quis urna ultrices, convallis eros eget, eleifend massa. Integer at leo erat. Etiam volutpat id" +
-    "tortor varius suscipit. Phasellus et lacus nunc.\n" +
-    "Sed ac ultrices orci. Suspendisse ac massa at arcu aliquam lacinia eget non felis." +
-    "Praesent eget velit viverra, pulvinar urna id, finibus lacus. Aliquam congue sit amet libero in" +
-    "rutrum. Nulla bibendum rutrum bibendum. Integer eleifend dolor in finibus finibus. In tellus turpis," +
-    "maximus sit amet volutpat eu, luctus non orci. Fusce laoreet laoreet ullamcorper. Praesent ut ipsum" +
-    "sagittis, pulvinar risus eget, tincidunt nibh.",
+      "Seu pet está pronto para enfrentar os vilões invisíveis? Com o exame de Doenças Infecciosas, " +
+      "nossos especialistas entram em ação para combater as ameaças invisíveis à saúde do seu amiguinho. " +
+      "Eles realizam testes para detectar a presença de agentes infecciosos, como vírus, bactérias e parasitas, " +
+      "e avaliar a resposta do sistema imunológico. Essa batalha contra os invasores invisíveis garante " +
+      "que seu pet esteja protegido e pronto para viver uma vida cheia de saúde e alegria.",
 };
