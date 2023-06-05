cefdc89f0448f884fa0b9b1085d52c4d4e92861c
Igor <igoraraujobda@gmail.com>
2023-06-05 12:56:48 -0300

    ajustes em andamento

--- src/components/Services/page.jsx
+++ src/components/Services/page.jsx
@@ -4,8 +4,7 @@ import NavUtilities from "@/components/nav-utilities/page";
 import Specialities from "@/components/specialities/page";
 import { useState } from "react";
 import Styles from "./styles.sass";
-import Image from "next/image";
-import Cat_image from "../../assets/cat_image.png";
+
 
 export default function Services() {
   const [buttonClick, setButtonClick] = useState(false);
@@ -18,11 +17,6 @@ export default function Services() {
         <NavUtilities
           buttonClick={buttonClick}
           setButtonClick={setButtonClick}
-        />
-        <Image
-          src={Cat_image}
-          alt="Imagem de gatinho"
-          className={`${Styles.app_container_services_bg_image} app-container-services-bg-image`}
         />
         <div
           className={`${Styles.app_container_services_info} app-container-services-info`}
