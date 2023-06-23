c242518bc58ac1ccd89482802f59a6ab669a9d47
Igor <igoraraujobda@gmail.com>
2023-06-23 17:31:03 -0300

    implementação parcial de novos agendamentos

--- src/components/NewSchedule/page.jsx
+++ src/components/NewSchedule/page.jsx
@@ -1,30 +1,91 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import style from './NewSchedule.module.scss'
+import userAndPetRegisterMock from '../../utils/userAndPetRegisterMock.jsx';
 
-const NewSchedule = () => { 
+const NewSchedule = () => {
 
-  const [ selectedSpecie, setSelectedSpecie ] = React.useState(undefined);
+  const [ userNamesArray, setUserNamesArray ] = useState([]);
+  const [ showUserNames, setShowUserNames ] = useState(false);
+  const [ userName, setUserName ] = useState('');
+  const [ selectedUserName, setSelectedUserName ] = useState('');
+  const [ petNamesArray, setPetNamesArray ] = useState([]);
 
-  const getSpecie = (event) => {
-    const specie = event.target.value;
-    if ( specie === undefined) {
-      setSelectedSpecie(undefined);
-    } else {
-      setSelectedSpecie(specie);
-    }
-  };
+  const getRegisteredUsers = (event) => {
+   const userData = event.target.value;
+    setUserName(userData);
+    setShowUserNames(true);
+    const foundUserNames = userAndPetRegisterMock.filter((user) => user.clientName.includes(userName));
+    setUserNamesArray(foundUserNames.map((user) => user.clientName));
+    userData === '' && setShowUserNames(false);
+   };
 
+   const getRegisteredPets = () => {
+    const foundUser = userAndPetRegisterMock.find((user) => user.clientName === selectedUserName);
+    console.log('foundUser', foundUser)
+    // const foundPetNames = foundUser[0].pets.map((pet) => pet.name);
+    // setPetNamesArray(foundPetNames);
+    };
+
+    const getSelectedUserName = (userName) => {
+      setSelectedUserName(userName);
+      setShowUserNames(false);
+      getRegisteredPets();
+    };
+
+    useEffect(() => {
+      getRegisteredPets();
+    }, [selectedUserName]);
+  
   return (
    <section className={style.fullComponent}>
       <div className={style.topDiv}>
-        <label for="GET-name">Tutor:</label>
-        <input id="GET-name" type="text" name="name"/>
+        <label htmlFor="userName">Tutor:</label>
+        <input
+          type="text"
+          id="userName" 
+          name="userName"
+          onChange={(e) => getRegisteredUsers(e)}
+          value= { document.getElementById('userName') && document.getElementById('userName').value}
+        />
+        {
+          showUserNames &&
+          <ul className={style.userNames} >
+            {
+              userNamesArray.map((userName) => (
+                <li
+                  key={userName}
+                  className={style.userName}
+                  onClick={() => getSelectedUserName(userName)}
+                >
+                  {userName}
+                </li>
+              ))
+            }
+          </ul> 
+        }
 
-        <label for="GET-petName">Pet:</label>
-        <input id="GET-petName" type="text" name="petName"/>
+        <label htmlFor="GET-petName">Pet:</label>
+        <select
+          id="GET-petName"
+          type="text"
+          name="petName"
+        >
+          <option id="" value="">Selecionar</option>
+          {
+            petNamesArray.map((petName) => (
+              <option
+                key={petName}
+                id={petName}
+                value={petName}
+              >
+                {petName}
+              </option>
+            ))
+          }
+       </select>
       </div>
       <div className={style.topDiv}>
-        <label for="GET-especialidade">Especialidade:</label>
+        <label htmlFor="GET-especialidade">Especialidade:</label>
         {/* Depois pegar da api de especialidades */}
         <select id="especialidade">
               <option id="" value="">Selecionar</option>
@@ -36,9 +97,9 @@ const NewSchedule = () => {
             </select>
       </div>
       <div className={style.topDiv}>
-        <label for="GET-date">Data:</label>
+        <label htmlFor="GET-date">Data:</label>
         <input id="GET-name" type="date" name="name"/>
-        <label for="GET-peso">Hora:</label>
+        <label htmlFor="GET-peso">Hora:</label>
         <input id="GET-peso" type="time" name="peso"/>
       </div>
       <div>
---
+++ src/utils/userAndPetRegisterMock.jsx
@@ -0,0 +1,54 @@
+const userAndPetRegisterMock = [
+  {
+   registrationId: 1,
+   clientName: 'Anna Maria',
+   phone: '123456789',
+   email: 'email@email.com',
+   cpf: '12345678912',
+   pets: [
+    {
+     name: 'Jonny',
+     specie: 'Cachorro',
+     breed: 'Vira-lata',
+     age: '2',
+     weight: '10',
+     sex: 'Macho',
+    },
+    {
+     name: 'Teddy',
+     specie: 'Gato',
+     breed: 'Vira-lata',
+     age: '2',
+     weight: '3',
+     sex: 'Macho',
+    }
+   ]
+  },
+  {
+   registrationId: 2, 
+   clientName: 'Ivete Sangalo',
+   phone: '123456789',
+   email: 'marie@email.com',
+   cpf: '12345678912',
+   pets: [
+    {
+     name: 'Billy',
+     specie: 'Cachorro',
+     breed: 'Vira-lata',
+     age: '2',
+     weight: '10',
+     sex: 'Macho',
+    },
+    {
+     name: 'Max',
+     specie: 'Gato',
+     breed: 'Vira-lata',
+     age: '1',
+     weight: '3',
+     sex: 'Macho',
+    },
+   ]
+  }
+]
+
+export default userAndPetRegisterMock;
