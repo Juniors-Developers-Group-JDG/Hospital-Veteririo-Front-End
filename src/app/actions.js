2991458616960ee4cfe4c8deae5d02a35e3c6a27
JamDev0 <jamdeveloper0@gmail.com>
2023-07-07 15:22:07 -0300

    fix: actions file type

---
+++ src/app/actions.js
@@ -0,0 +1,23 @@
+'use server';
+
+import { cookies } from 'next/headers';
+
+export const createCookie = async (
+  key,
+  value,
+  maxAgeInMs = 60 * 60 * 12, // 12 hour
+) => {
+  cookies().set(`vet.${key}`, value, {
+    maxAge: maxAgeInMs,
+  });
+};
+
+export const deleteCookie = async key => {
+  // @ts-ignore
+  cookies().set(`vet.${key}`, '', {
+    maxAge: 0,
+  });
+};
+
+export const getCookie = async key =>
+  cookies().get(`vet.${key}`)?.value;
