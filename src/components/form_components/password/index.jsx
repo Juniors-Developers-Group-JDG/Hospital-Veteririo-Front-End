5a347bba14ad4d6cb638ec67dc732458934089c3
JamDev0 <jamdeveloper0@gmail.com>
2023-07-07 14:43:26 -0300

    removed: out of scope functionalities from password component

--- src/components/form_components/password/index.jsx
+++ src/components/form_components/password/index.jsx
@@ -1,16 +1,11 @@
-import { useContext } from 'react';
 import style from './password.module.scss';
-import AuthContext from '../../../app/contexts/auth_context/AuthContext';
 
-export default function Password() {
-  const { password, setPassword } = useContext(AuthContext);
+export default function Password({ ...props }) {
   return (
       <div className={style.box}>
         <input
+          {...props}
           type="password"
-          placeholder='Senha'
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
         />
       </div>
     
