06e507336734bcb76de7977ae3acbef953014b21
JamDev0 <jamdeveloper0@gmail.com>
2023-08-28 21:44:42 -0300

    feature: isAuthenticated validation made in inner layout instead of only on admin page

--- src/app/(in)/admin/page.jsx
+++ src/app/(in)/admin/page.jsx
@@ -3,14 +3,8 @@ import ScheduleList from '@/components/ScheduleList';
 import { ScheduleProvider } from '@/contexts/schedule_context';
 import style from './page.module.scss';
 
-import { cookies } from 'next/headers';
-import { redirect } from 'next/navigation';
 
 export default function Admin() {
-  const isAuthenticated = cookies().get('vet.authenticated')?.value;
-
-  if (!isAuthenticated) redirect('/login')
-
   return (
     <div className={style.adminPage}>
       <ScheduleProvider>
--- src/app/(in)/layout.jsx
+++ src/app/(in)/layout.jsx
@@ -1,9 +1,16 @@
+import { getCookie } from "../actions";
 import { InnerHeader } from "./components/Header";
 import { Sidebar } from "./components/Sidebar";
+
+import { redirect } from 'next/navigation';
 
 import Styles from './layout.module.sass';
 
 export default function InnerLayout({ children }) {
+  const isAuthenticated = getCookie('username');
+
+  if (!isAuthenticated) redirect('/login');
+
   return ( 
     <div className={Styles.LayoutContainer}>
       <Sidebar />
