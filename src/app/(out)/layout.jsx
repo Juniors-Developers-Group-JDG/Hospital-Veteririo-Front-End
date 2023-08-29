7fa392ea0125283959c59e788aa585ecaa5439de
JamDev0 <jamdeveloper0@gmail.com>
2023-08-28 21:45:47 -0300

    feature: isAuthenticated validation in outer layout

--- src/app/(out)/layout.jsx
+++ src/app/(out)/layout.jsx
@@ -1,6 +1,10 @@
 import NavBar from "../../components/nav_bar/page";
 
 export default function RootLayout({ children }) {
+  const isAuthenticated = getCookie('username');
+
+  if (isAuthenticated) redirect('/admin');
+
   return (
     <>
       <NavBar />
