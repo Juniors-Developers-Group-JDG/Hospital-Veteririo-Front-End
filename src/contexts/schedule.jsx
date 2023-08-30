318a122c01eac3dbdf992f8d81f2ca51b49882db
JamDev0 <jamdeveloper0@gmail.com>
2023-08-30 12:55:00 -0300

    feat: schedule context and hook

---
+++ src/contexts/schedule.jsx
@@ -0,0 +1,60 @@
+'use client';
+
+import { isSameDay, isSameMonth } from "date-fns";
+import { zonedTimeToUtc } from "date-fns-tz";
+import { useMemo } from "react";
+
+const { createContext } = require("react");
+
+export const scheduleContext = createContext({});
+
+export function ScheduleProvider({ children }) {
+  const [schedules, setSchedules] = useState();
+
+  const [token, setToken] = useState('');
+
+  const monthTotalSchedules = useMemo(() => schedules 
+    ? 
+      schedules.filter(schedule => {
+        const utcToday = zonedTimeToUtc(new Date()); 
+        return isSameMonth(schedule.scheduleDate, utcToday)
+      }).length
+    : 
+      0, 
+    [schedules],
+  )
+
+  const todayTotalSchedules = useMemo(() => schedules 
+    ? 
+      schedules.filter(schedule => {
+        const utcToday = zonedTimeToUtc(new Date()); 
+        return isSameDay(schedule.scheduleDate, utcToday)
+      }).length
+    : 
+      0, 
+    [schedules],
+  )
+
+  useEffect(() => {
+    getCookie('token').then(cookie => setToken(cookie));
+  }, [])
+
+  useEffect(() => {
+    if(token) {
+      fetch('https://jdg-site-vet.onrender.com/schedules/getAll', { 
+        method: 'GET', 
+        headers: {
+          Authorization: `Bearer ${token}`
+        } 
+      })
+      .then(res => res.json())
+      .then(data => setSchedules(data));
+    }
+  }, [token])
+  
+  return (
+    <scheduleContext.Provider value={{schedules, monthTotalSchedules, todayTotalSchedules}}>
+      {children}
+    </scheduleContext.Provider>
+  )
+}
