4e56885317a7b29a29b369df0992afaf445ab564
JamDev0 <jamdeveloper0@gmail.com>
2023-08-30 11:54:42 -0300

    feat: removed redirect if logged in user trys to access all  outer routes

--- src/app/(out)/layout.jsx
+++ src/app/(out)/layout.jsx
@@ -1,12 +1,6 @@
-import { redirect } from "next/navigation";
-import { getCookie } from "../actions";
 import { OuterNavBar } from "./components/NavBar";
 
-export default function RootLayout({ children }) {
-  const isAuthenticated = getCookie('username');
-
-  if (isAuthenticated) redirect('/admin');
-
+export default async function RootLayout({ children }) {
   return (
     <>
       <OuterNavBar />
