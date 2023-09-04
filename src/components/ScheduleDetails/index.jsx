9f5cd1d1162655c39f2e08ecaa945ecc8683bc83
JamDev0 <jamdeveloper0@gmail.com>
2023-09-04 17:51:15 -0300

    feat: initial ScheduleDetails backend integration

--- src/components/ScheduleDetails/index.jsx
+++ src/components/ScheduleDetails/index.jsx
@@ -1,112 +1,86 @@
-import React from 'react';
+'use client'
+
 import { CgCloseO } from 'react-icons/cg';
 import style from './ScheduleDetails.module.scss';
 
-const ScheduleDetails = ({schedule, setSchedule}) => {
+export const ScheduleDetails = () => {
+  const { selectedSchedule } = useSchedule()
 
-const handleChangeInput = (event) => {
-  const { name, value } = event.target;
-  setSelectedSchedule({
-    ...selectedSchedule,
-    [name]: value,
-  });
-};
+  const handleChangeInput = (event) => {
+  };
 
-const saveEditedSchedule = () => {
-  const newSchedule = schedule.map((eachSchedule) => {
-    if (eachSchedule.id === selectedSchedule.id) {
-      return selectedSchedule;
-    }
-    return eachSchedule;
-  });
-  setSchedule(newSchedule);
-  setShowScheduleDetails(false);
-  setSelectedSchedule({});
-}
-
-const cancelSchedule = () => {
-  const newSchedule = schedule.filter((eachSchedule) => {
-    if (eachSchedule.id !== selectedSchedule.id) {
-      return eachSchedule;
-    }
-  });
-  setSchedule(newSchedule);
-  setShowScheduleDetails(false);
-  setSelectedSchedule({});
-};
-
-const confirmCancelSchedule = () => {
-  const confirm = window.confirm('Tem certeza que deseja cancelar o agendamento?');
-  if (confirm) {
-    cancelSchedule();
+  const saveEditedSchedule = () => {
   }
-};
 
- return (
-  <section className={style.scheduleDetailContainer}>
-    <form className={style.form}>
-    <div className={ style.boxLine}>
-      <div className={style.boxInput}>
-        <label htmlFor="clientName">Nome do cliente</label>
-        <input
-          type="text"
-          name="clientName"
-          id="clientName"
-          value={ selectedSchedule.clientName }
-          onChange={handleChangeInput}
-        />
-      </div>
-      <div className={style.boxInput}>
-        <label htmlFor="petName">Nome do Pet</label>
-        <input type="text" value={selectedSchedule.pet} name='pet' id='pet' onChange={handleChangeInput}/>
-      </div>
-     </div>
-     <div className={ style.boxLine}>
-     <div className={style.boxInput}>
-        <label htmlFor="date">Data</label>
-        <input type="date" value={selectedSchedule.date} name='date' id='date'onChange={handleChangeInput}/>
-      </div>
-      <div className={style.boxInput}>
-        <label htmlFor="time">Horário</label>
-        <input type="time" value={selectedSchedule.time} name='time' id='time'onChange={handleChangeInput}/>
-      </div>
-     </div>
-     <div className={ style.boxLine}>
-      <div className={style.boxInput}>
-        <label htmlFor="speciality">Especialidade</label>
-        <input type="text" value={selectedSchedule.speciality} name='speciality' id='speciality'onChange={handleChangeInput}/>
-      </div>
-      <div className={style.boxInput}>
-        <label htmlFor="status">Resumo</label>
-        <textarea name="notes" id="notes" cols="30" rows="5" value={selectedSchedule.notes} onChange={handleChangeInput}></textarea>
-      </div>
-     </div>
-     <p
-      className={style.close}
-      onClick={() => {
-       setShowScheduleDetails(false);
-       setSelectedSchedule({});
-      }}
-     >
-      <CgCloseO />
-     </p>
-   </form>
-   <div className={style.buttons}>
-    <button
-      type='button'
-      onClick={saveEditedSchedule}
-    >
-     Salvar
-    </button>
-    <button
-      type='button'
-      onClick={confirmCancelSchedule}
-    >
-     Cancelar agendamento
-    </button>
-   </div>
-  </section>
- );
-};
+  const cancelSchedule = () => {
+  };
 
-export default ScheduleDetails;
+  const confirmCancelSchedule = () => {
+  };
+
+  return (
+    <section className={style.scheduleDetailContainer}>
+      <form className={style.form}>
+      <div className={ style.boxLine}>
+        <div className={style.boxInput}>
+          <label htmlFor="clientName">Nome do cliente</label>
+          <input
+            type="text"
+            name="clientName"
+            id="clientName"
+            value={ selectedSchedule.clientName }
+            onChange={handleChangeInput}
+          />
+        </div>
+        <div className={style.boxInput}>
+          <label htmlFor="petName">Nome do Pet</label>
+          <input type="text" value={selectedSchedule.pet} name='pet' id='pet' onChange={handleChangeInput}/>
+        </div>
+      </div>
+      <div className={ style.boxLine}>
+      <div className={style.boxInput}>
+          <label htmlFor="date">Data</label>
+          <input type="date" value={selectedSchedule.date} name='date' id='date'onChange={handleChangeInput}/>
+        </div>
+        <div className={style.boxInput}>
+          <label htmlFor="time">Horário</label>
+          <input type="time" value={selectedSchedule.time} name='time' id='time'onChange={handleChangeInput}/>
+        </div>
+      </div>
+      <div className={ style.boxLine}>
+        <div className={style.boxInput}>
+          <label htmlFor="speciality">Especialidade</label>
+          <input type="text" value={selectedSchedule.speciality} name='speciality' id='speciality'onChange={handleChangeInput}/>
+        </div>
+        <div className={style.boxInput}>
+          <label htmlFor="status">Resumo</label>
+          <textarea name="notes" id="notes" cols="30" rows="5" value={selectedSchedule.notes} onChange={handleChangeInput}></textarea>
+        </div>
+      </div>
+      <p
+        className={style.close}
+        onClick={() => {
+        setShowScheduleDetails(false);
+        setSelectedSchedule({});
+        }}
+      >
+        <CgCloseO />
+      </p>
+    </form>
+    <div className={style.buttons}>
+      <button
+        type='button'
+        onClick={saveEditedSchedule}
+      >
+      Salvar
+      </button>
+      <button
+        type='button'
+        onClick={confirmCancelSchedule}
+      >
+      Cancelar agendamento
+      </button>
+    </div>
+    </section>
+  );
+};
