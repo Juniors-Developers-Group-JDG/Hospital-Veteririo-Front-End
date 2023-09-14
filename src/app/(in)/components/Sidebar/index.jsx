f12973978bf5867ea2660e58ee156edc9ddd6fee
JamDev0 <jamdeveloper0@gmail.com>
2023-09-14 13:35:10 -0300

    feat: sidebar functionalities

--- src/app/(in)/components/Sidebar/index.jsx
+++ src/app/(in)/components/Sidebar/index.jsx
@@ -1,11 +1,17 @@
-import { GiCalendar } from 'react-icons/gi'
+import { HiCalendarDays, HiComputerDesktop } from 'react-icons/hi2'
 
+import Link from 'next/link'
 import Styles from './Sidebar.module.sass'
 
 export function Sidebar() {
   return (
     <aside className={Styles.SidebarContainer}>
-      <GiCalendar className={Styles.SidebarIcon} />
+      <Link href='/admin'>
+        <HiComputerDesktop className={Styles.SidebarIcon} />
+      </Link>
+      <Link href='/schedule'>
+        <HiCalendarDays className={Styles.SidebarIcon} />
+      </Link>
     </aside>
   )
 }
