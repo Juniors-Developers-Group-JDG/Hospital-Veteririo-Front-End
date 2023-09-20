cefad35578077980e5eec58b67baea12d2c241ed
JamDev0 <jamdeveloper0@gmail.com>
2023-09-20 17:19:16 -0300

    fix: login page overflow

--- src/app/(out)/layout.jsx
+++ src/app/(out)/layout.jsx
@@ -1,10 +1,11 @@
 import { OuterNavBar } from "./components/NavBar";
+import Styles from './OuterLayout.module.sass';
 
 export default async function RootLayout({ children }) {
   return (
-    <>
+    <div className={Styles.OuterLayoutContainer}>
       <OuterNavBar />
       {children}
-    </>
+    </div>
   );
 }
